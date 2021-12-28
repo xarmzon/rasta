74f0807e76a4e73121b894230f930fc68a831a97
unknown <xarmzon@gmail.com>
2021-12-28 16:28:00 +0000

    update LinkIcon social

--- components/LinkIcon.tsx
+++ components/LinkIcon.tsx
@@ -4,11 +4,13 @@ import { ILinkIcon } from "../types";
 
 const LinkIcon = ({ url, icon }: ILinkIcon) => {
   return (
-    <Link href={url}>
-      <a className="text-primary-100 bg-primary hover:bg-primary-100 hover:text-primary hover:rotate(20deg) h-6 w-6 text-sm md:text-lg md:h-8 md:w-8 rounded-full flex items-center justify-center transition-all duration-500">
-        {icon}
-      </a>
-    </Link>
+    <a
+      href={url}
+      target="_blank"
+      className="text-primary-100 bg-primary hover:bg-primary-100 hover:text-primary hover:rotate(20deg) h-6 w-6 text-sm md:text-lg md:h-8 md:w-8 rounded-full flex items-center justify-center transition-all duration-500"
+    >
+      {icon}
+    </a>
   );
 };
 
