55d09598d6d79ada1d9f346bd3b8bf21e227eb4c
unknown <xarmzon@gmail.com>
2021-12-29 01:34:10 +0000

    added nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,36 @@
-import "../styles/globals.css";
-
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import { DefaultSeo } from "next-seo";
 import { DEFAULT_TITLE, DESCRIPTION } from "../libs/constants";
 import { AnimatePresence, motion } from "framer-motion";
 import Contact from "../components/Contact";
+import NProgress from "nprogress";
+import { useRouter } from "next/router";
+
+import "nprogress/nprogress.css";
+import "../styles/globals.css";
+
+NProgress.configure({ showSpinner: false });
 
 function MyApp({ Component, pageProps, router }: AppProps) {
+  //const router2 = useRouter();
+
+  useEffect(() => {
+    const startProgress = () => NProgress.start();
+    const stopProgress = () => NProgress.done();
+
+    router.events.on("routeChangeStart", startProgress);
+    router.events.on("routeChangeComplete", stopProgress);
+    router.events.on("routeChangeError", stopProgress);
+
+    return () => {
+      router.events.off("routeChangeStart", startProgress);
+      router.events.off("routeChangeComplete", stopProgress);
+      router.events.off("routeChangeError", stopProgress);
+    };
+  }, [router]);
   const [navOpen, setNavOpen] = useState<boolean>(false);
   const closeNav = () => navOpen && setNavOpen((prev) => false);
   return (
